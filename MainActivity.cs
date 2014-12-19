c8b8848ad7aae0c7dd62ef1d4d475128a00fca38
Koteyko <nkurgin@yandex.ru>
2014-12-19 19:58:19 +0600

    Проект
    Каркас проекта

---
+++ MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace healthy_eating
+{
+	[Activity (Label = "Healthy Eating", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		//int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.main);
+
+			/*
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.button1);
+			
+			button.Click += delegate
+			{
+				button.Text = "";
+			};
+			*/
+		}
+	}
+}
+
+
