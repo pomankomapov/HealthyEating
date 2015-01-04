3fe25a2f61471090b5de7080a8a0bd6e7ae3419b
Koteyko <nkurgin@yandex.ru>
2015-01-04 17:52:09 +0600

    Глобальные переменные приложения
    Такие как ID профиля пользователя и ID устройства

---
+++ Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace healthy_eating
+{
+    // Глобальное пространство переменных
+    //  доступно из любого активити
+
+    public static class Global
+    {
+        public static int userID { get; set; }       // ID пользователя этого устройства
+        public static string deviceID { get; set; }  // ID устройства
+    }
+}
+
