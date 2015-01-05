5dddd0ac9218dedffa6982e9ff17e5e32bc44801
Koteyko <nkurgin@yandex.ru>
2015-01-05 16:28:45 +0600

    Добавлены тосты
    Toast - небольшие сообщения

--- Global.cs
+++ Global.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Android.Content;
+using Android.Widget;
 
 namespace healthy_eating
 {
@@ -9,6 +11,11 @@ namespace healthy_eating
     {
         public static int userID { get; set; }       // ID пользователя этого устройства
         public static string deviceID { get; set; }  // ID устройства
+
+        public static void print(Android.Content.Context context, string str)
+        {
+            Android.Widget.Toast.MakeText(context, str, ToastLength.Short).Show();
+        }
     }
 }
 
